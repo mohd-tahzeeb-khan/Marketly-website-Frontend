b815d89272f8055fe1941a0f60c15a1b237f0cc4
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-11-03 19:16:44 +0530

    common font file.

---
+++ marketly/src/app/font.js
@@ -0,0 +1,14 @@
+import {Roboto_Slab} from 'next/font/google'
+
+export const Roboto_Slab_font_bold=Roboto_Slab({
+    subsets:['latin'],
+    weight:'800',
+    style:'normal'
+
+})
+export const Roboto_Slab_font_light=Roboto_Slab({
+    subsets:['latin'],
+    weight:'500',
+    style:'normal'
+
+})
