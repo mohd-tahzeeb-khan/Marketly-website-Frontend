d76796aed35aedfa3485f2b53e0d0bba03e19125
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-11-03 20:28:09 +0530

    Cards section is Added to the Page.js

--- marketly/src/app/page.js
+++ marketly/src/app/page.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from './components/header'
 import Home from './sections/landing/home'
+import Card from './sections/cards/cards'
 const page = () => {
   return (
    <>
    <Header />
    <Home />
+   <Card />
    </>
   )
 }
