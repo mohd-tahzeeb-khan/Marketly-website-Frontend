cb38c65a1143b2cb79a10855ab333713dee72f41
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-11-04 23:49:26 +0530

    added some components and section to the main page of the project

--- marketly/src/app/page.js
+++ marketly/src/app/page.js
@@ -1,13 +1,16 @@
 import React from 'react'
-import Header from './components/header'
 import Home from './sections/landing/home'
 import Card from './sections/cards/cards'
+import Contact from './sections/contact/contact'
+import Stretergy from './sections/stretergy/stretergy'
 const page = () => {
   return (
    <>
-   <Header />
+  
    <Home />
    <Card />
+   <Stretergy />
+   <Contact />
    </>
   )
 }
