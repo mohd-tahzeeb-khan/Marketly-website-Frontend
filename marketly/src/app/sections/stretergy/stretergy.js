9d0deab85b2a33aacd354833a1e0f804d84dcbea
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-11-04 23:52:38 +0530

    section is ready

---
+++ marketly/src/app/sections/stretergy/stretergy.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const stretergy = () => {
+  return (
+    <>
+     <div className='h-[100vh] bg-cyan-500 border-2 border-green-500'></div>
+    </>
+  )
+}
+
+export default stretergy
