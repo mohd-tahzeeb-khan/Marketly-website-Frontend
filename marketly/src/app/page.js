7919be187e7696517de99357842dc960855c7ad3
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-11-06 01:08:28 +0530

    added new section to the project named "Serices"

--- marketly/src/app/page.js
+++ marketly/src/app/page.js
@@ -3,12 +3,14 @@ import Home from './sections/landing/home'
 import Card from './sections/cards/cards'
 import Contact from './sections/contact/contact'
 import Stretergy from './sections/stretergy/stretergy'
+import Services from './sections/choose/choose'
 const page = () => {
   return (
    <>
   
    <Home />
    <Card />
+   <Services />
    <Stretergy />
    <Contact />
    </>
