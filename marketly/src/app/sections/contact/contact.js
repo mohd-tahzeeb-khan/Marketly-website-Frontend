b5f323b38c0dd70a49e96c64e40f7739f2c80d5a
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-11-05 23:22:15 +0530

    edited some code for better performance

--- marketly/src/app/sections/contact/contact.js
+++ marketly/src/app/sections/contact/contact.js
@@ -4,10 +4,10 @@ const contact = () => {
   return (
     <>
        
-        <div className='w-[90vw] h-[50vh] mx-auto my-auto bg-colorgreen flex flex-col rounded-tl-3xl rounded-tr-3xl mt-7'>
+        <div className='w-[90vw] h-[60vh] mx-auto my-auto bg-colorgreen flex flex-col rounded-tl-3xl rounded-tr-3xl mt-7 py-20'>
             <div className='bg-transparent'>
-                <h1 className='text-white uppercase text-8xl bg-transparent pl-56'>Let's Make</h1>
-                <h1 className='text-white uppercase text-8xl bg-transparent pl-56'>Something great!</h1>
+                <h1 className='text-white uppercase text-8xl bg-transparent pl-56 font-bold'>Let's Make</h1>
+                <h1 className='text-white uppercase text-8xl bg-transparent pl-56 font-bold'>Something great!</h1>
             </div>
             <div className=' bg-transparent pl-60 mt-10'>
                 <button className='bg-colorbrown text-white after:content-[">"] text-3xl p-6 rounded-full hover:scale-105 hover:duration-150 hover:bg-colorbrown'>Say H!     </button>
